3f26967c20168758dedd292cf108adf99ef1957f
harddy0 <kingstonharddycanales@gmail.com>
2026-04-28 14:40:36 +0800

    db

---
+++ apps/api/prisma/migrations/20260428061545_yeah/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `yeah` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `yeah2` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `yeah`;
+
+-- DropTable
+DROP TABLE `yeah2`;
